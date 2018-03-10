22acb7d9fb3e48d3e8832f588ab0c6c322c92543
Lakatos Martin <dhumdil@gmail.com>
2018-03-10 20:55:49 +0200

    [+] ex00

--- day_04/ex00/index.php
+++ day_04/ex00/index.php
@@ -1,5 +1,38 @@
 <?php
+
 	session_start();
+
+	$_SESSION['is_set_login'] = 0;
+	$_SESSION['is_set_passwd'] = 0;
+
+	foreach ($_GET as $key => $value)
+	{
+		if ($key == 'login')
+		{
+			$_SESSION['login'] = $value;
+			$_SESSION['is_set_login'] = 1;
+		}
+		if ($key == 'passwd')
+		{
+			$_SESSION['passwd'] = $value;
+			$_SESSION['is_set_passwd'] = 1;
+		}
+	}
+
+	foreach ($_SESSION as $key => $value)
+	{
+		if ($key == 'login')
+		{
+			$_SESSION['login'] = $value;
+			$_SESSION['is_set_login'] = 1;
+		}
+		if ($key == 'passwd')
+		{
+			$_SESSION['passwd'] = $value;
+			$_SESSION['is_set_passwd'] = 1;
+		}
+	}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,20 +40,17 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Rush00 - PHP Piscine | Academy+Plus">
 
 	<title>Session</title>
-
-	<link href="src/css/btn.css" type="text/css" rel="stylesheet">
 </head>
 <body>
 	<form id="index.php" method="GET" action="index.php">
 
 		<label for="login">Username:</label>
-		<input type="text" name="login" value="<?php echo (isset($SESSION['login'])) ? $SESSION['login'] : ''; ?>" required autofocus>
+		<input type="text" name="login" value="<?php echo ($_SESSION['is_set_login'] ? $_SESSION['login'] : ''); ?>" autofocus>
 
 		<label for="passwd">Password:</label>
-		<input type="text" name="passwd" value="<?php echo (isset($SESSION['passwd'])) ? $SESSION['passwd'] : ''; ?>" required>
+		<input type="password" name="passwd" value="<?php echo ($_SESSION['is_set_passwd'] ? $_SESSION['passwd'] : ''); ?>">
 
 		<input name="submit" type="submit" value="OK">
 
